093ad5ca21cd1b89788a03a726fd4c5724cd84d9
pragus99 <prayogo0399@gmail.com>
2021-03-05 08:38:05 +0700

    created and change controller and model category

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.dashboard');
 });
+
+Route::get('category', CategoryController::class);
 
 Auth::routes();
 
